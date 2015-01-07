1121ce1ffac161bff6309faaa0cdb798014a969a
Ryan Jarrett <ryan.jarrett@digital.justice.gov.uk>
2015-01-07 10:51:37 +0000

    Created need to know widget that is managed from customiser

--- need_to_know_widget.php
+++ need_to_know_widget.php
@@ -1,14 +1,12 @@
 <?php
 /*
-Plugin Name: DW Sticky news
-Description: Display sticky news
-*/
-
-error_reporting(E_ALL ^ E_NOTICE);
-ini_set('display_errors', 1);
+ *
+ * Need to know widget
+ *
+ **/
 
 if (class_exists('mmvc')) {
-  class DW_sticky_news_controller extends MVC_controller {
+  class need_to_know_controller extends MVC_controller {
     function __construct($args, $instance){
       $this->args = $args;
       $this->instance = $instance;
@@ -18,15 +16,18 @@ if (class_exists('mmvc')) {
 
     function main(){
       $title = apply_filters('widget_title', $this->instance['title']);
-      $items = intval($this->instance['items']);
+
+      // Get stored story IDs (set in customizer)
+      $need_to_know_story1 = get_option('need_to_know_story1');
+      $need_to_know_story2 = get_option('need_to_know_story2');
+      $need_to_know_story3 = get_option('need_to_know_story3');
 
       $cquery = array(
-        'orderby'=>'post_date',
-        'order'=>'DESC',
-        'post_type'=>'news',
-        'posts_per_page'=>$items,
-        'meta_key'=>'news_listing_type',
-        'meta_value'=>'1'
+        'orderby'         =>'post_date',
+        'order'           =>'DESC',
+        'post_type'       =>'news',
+        'posts_per_page'  => 3,
+        'post__in'        => array($need_to_know_story1,$need_to_know_story2,$need_to_know_story3)
       );
 
       $news = new WP_Query($cquery);
@@ -65,41 +66,34 @@ if (class_exists('mmvc')) {
     }
   }
 
-  class DW_sticky_news extends WP_Widget{
-    function DW_sticky_news(){
-      parent::WP_Widget(false, 'DW Sticky news', array('description' => 'Sticky news widget'));
+  class need_to_know_widget extends WP_Widget{
+    function need_to_know_widget(){
+      parent::WP_Widget(false, 'Need to know', array('description' => '"Need to know" widget'));
     }
 
     function widget($args, $instance){
-      new DW_sticky_news_controller($args, $instance);
+      new need_to_know_controller($args, $instance);
     }
 
     function update($new_instance, $old_instance){
       $instance = $old_instance;
       $instance['title'] = strip_tags($new_instance['title']);
-      $instance['items'] = strip_tags($new_instance['items']);
 
       return $instance;
     }
 
     function form($instance) {
       $title = esc_attr($instance['title']);
-      $items = esc_attr($instance['items']);
       ?>
       <p>
         <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
         <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /><br><br>
-
-        <label for="<?php echo $this->get_field_id('items'); ?>"><?php _e('Number of items:'); ?></label>
-        <input class="widefat" id="<?php echo $this->get_field_id('items'); ?>" name="<?php echo $this->get_field_name('items'); ?>" type="text" value="<?php echo $items; ?>" /><br><br>
       </p>
       <?php
     }
   }
 
-  add_action('widgets_init', create_function('', 'return register_widget("DW_sticky_news");'));
+  add_action('widgets_init', create_function('', 'return register_widget("need_to_know_widget");'));
 }
-
-include_once('need_to_know_widget.php');
 
 ?>
